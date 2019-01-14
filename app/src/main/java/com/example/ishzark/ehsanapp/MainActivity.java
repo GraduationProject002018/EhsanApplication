afb7322c0b57637f780c0c07a8fbedb65c938c62
mariam <mariam.alrubei@gmail.com>
2019-01-14 23:03:23 +0300

    google firebase authentication in gradle

--- app/src/main/java/com/example/ishzark/ehsanapp/MainActivity.java
+++ app/src/main/java/com/example/ishzark/ehsanapp/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.loadingscreen);
+        setContentView(R.layout.login);
     }
 }
